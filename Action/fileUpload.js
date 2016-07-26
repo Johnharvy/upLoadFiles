a1400394d6347f65054f65993f7ded2b9c5d4e4b
Johnharvy <1667582186@qq.com>
2016-07-26 11:34:18 +0800

    v1.0

---
+++ Action/fileUpload.js
@@ -0,0 +1,34 @@
+var multipart = require('connect-multiparty');
+var App=require("../app");
+var path = require('path');
+var fs=require("fs");
+var app=App.app;
+
+var uploadTest={};
+
+function uploadFile(){
+    app.post("/uploadFile", multipart(),function(req,res) {
+
+       var i=0;
+       while(i != null){
+           if(req.files["file"+i]) upLoad(i);
+            else{ i= null; res.json({infor:"success"});return;}
+           i++;
+       }
+        //上传队列文件
+       function upLoad(index){
+            var filename = req.files["file"+index].originalFilename || path.basename(req.files["file"+index].path);
+
+            //path接口可以指定文件的路径和文件名称，"\结尾默认为路径，字符串结尾默认为文件名"
+            var targetPath = path.dirname("") + '/public/uploadFiles/' + filename;
+
+            //fs创建指定路径的文件并将读取到的文件内容写入
+            fs.createReadStream(req.files["file"+index].path).pipe(fs.createWriteStream(targetPath));
+         }
+
+    });
+}
+
+uploadTest.uploadFile=uploadFile;
+
+exports.uploadTest=uploadTest;
