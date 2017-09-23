e4721f8e850206f63b1567aa401043bd067ff0dc
Johnharvy <1667582186@qq.com>
2017-09-23 08:39:55 +0800

    -

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-// app.use(express.methodOverride());
+app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname)));
 
